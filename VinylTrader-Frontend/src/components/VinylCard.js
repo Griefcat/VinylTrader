5fb798f400f02d374189a9de5fbefe61393f5c99
Griefcat <anardolilli@gmail.com>
2021-02-25 11:03:18 -0500

    initial cards on screen

--- VinylTrader-Frontend/src/components/VinylCard.js
+++ VinylTrader-Frontend/src/components/VinylCard.js
@@ -1,11 +1,66 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+// import { Card } from '@material-ui/core'
 
-export default class VinylCard extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+// export default class VinylCard extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h3>this is a card</h3>
+//                 <Card> It'sa me, Mario </Card> 
+//             </div>
+//         )
+//     }
+// }
+
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+
+export default function ImgMediaCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="340"
+          image='https://m.media-amazon.com/images/I/617rSnmzb3L._SS500_.jpg'
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
--- VinylTrader-Frontend/src/containers/VinylContainer.js
+++ VinylTrader-Frontend/src/containers/VinylContainer.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
 import VinylCard from '../components/VinylCard'
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from "@material-ui/core"
+
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+  });
+
 
 export default class VinylContainer extends Component {
+    
     render() {
         return (
             <div>
-                <h2>hey</h2>
-                <>
+                <Grid container spacing={4}>
+                    <Grid item >
+                        <VinylCard/>
+                    </Grid>
+                    <Grid item >
+                        <VinylCard/>
+                    </Grid>
+                    <Grid item >
+                        <VinylCard/>
+                    </Grid>
+                </Grid>
                 
-                </>
+                
+                
             </div>
         )
     }
